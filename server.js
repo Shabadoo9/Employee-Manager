cc3ee5cb450275c6d64882b8c44e920cd2c434c7
Shabadoo9 <brinstonzbpe@gmail.com>
2023-08-16 15:09:06 -0400

    added basic menu

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -9,3 +10,43 @@ const connection = mysql.createConnection({
     database: 'company_db'
   });
   
+
+  console.log(`
+    _____  ____   __  __  _____          _   _ __     __    
+  / ____|/ __ \\ |  \\/  ||  __ \\  /\\    | \\ | |\\ \\   / /    
+ | |    | |  | || \\  / || |__) |/  \\   |  \\| | \\ \\_/ /     
+ | |    | |  | || |\\/| ||  ___// /\\ \\  | . \` |  \\   /      
+ | |____| |__| || |  | || |   / ____ \\ | |\\  |   | |       
+  \\_____|\\____/ |_| _|_||_|  /_/    \\_\\|_| \\_|___|_|_____  
+ |  \\/  |    /\\    | \\ | |    /\\    / ____||  ____||  __ \\ 
+ | \\  / |   /  \\   |  \\| |   /  \\  | |  __ | |__   | |__) |
+ | |\\/| |  / /\\ \\  | . \` |  / /\\ \\ | | |_ ||  __|  |  _  / 
+ | |  | | / ____ \\ | |\\  | / ____ \\| |__| || |____ | | \\ \\ 
+ |_|  |_|/_/    \\_\\|_| \\_|/_/    \\_\\_____||______||_|  \\_\\
+
+ `)
+
+ connection.connect((err) => {
+    if (err) throw err;
+    console.log('connected to database')
+    menu();
+ });
+
+ function menu() {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'options',
+        message: 'How would you like to proceed',
+        Choices: [
+            'View all departments',
+            'View all roles',
+            'view all employees',
+            'add a department',
+            'add a role',
+            'add an employee',
+            'update an employee',
+            'exit',
+        ]
+    }
+    ])
+ }
