40f4754c65b3d83a6d5cfa3944f5e0d70fbfcde4
Shabadoo9 <brinstonzbpe@gmail.com>
2023-08-16 13:25:14 -0400

    completed schema.sql

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Missbean7!',
+    database: 'employees'
+  });
+  
